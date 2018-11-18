971f795d06bd7048ff4a810b64f6a0fe4d890b47
airua <auriavaci@gmail.com>
2018-11-18 23:09:53 +0100

    getting

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { ProveedorProvider } from '../../providers/proveedor/proveedor';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  //creo variable donde recoger datos para mostrarlos en html
+  //como no se indica nada, tipo any
+  usuarios
+  constructor(public navCtrl: NavController,
+              public proveedor: ProveedorProvider) {
 
   }
+  
+  ionViewDidLoad() {
+    this.proveedor.cargarDatos().subscribe((data)=>{this.usuarios=data;},
+                                            (error)=>{console.log(error);})
+  }
+  
 
 }
---
+++ src/providers/proveedor/proveedor.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+//importamos
+import { HttpClientModule } from '@angular/common/http';
+
+/*
+  Generated class for the ProveedorProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class ProveedorProvider {
+
+  constructor(public http: HttpClient) {
+    console.log('Hello ProveedorProvider Provider');
+  }
+
+cargarDatos()
+{
+  return this.http.get('https://jsonplaceholder.typicode.com/users');
+}
+
+}
